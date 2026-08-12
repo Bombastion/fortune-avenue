fde2d0f48ae99d7eefd0dc016b3d8554912944c9
Bombastion <tyler.tift@gmail.com>
2026-08-11 21:27:23 -0600

    Removing stupid comment

--- service/src/main/resources/db/migration/V12__add_starting_and_current_gold.sql
+++ service/src/main/resources/db/migration/V12__add_starting_and_current_gold.sql
@@ -6,8 +6,5 @@ ALTER TABLE boards
     ADD COLUMN starting_gold INT NOT NULL
         CONSTRAINT chk_boards_starting_gold_positive CHECK (starting_gold > 0);
 
--- No non-negative check: current_gold can go below zero if a player spends more than they have
--- on hand, which triggers a state where they have to auction off properties to get back in the
--- positive (auction mechanics aren't implemented yet, but the schema has to allow the state).
 ALTER TABLE player_states
     ADD COLUMN current_gold INT NOT NULL;
